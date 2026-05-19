3b9960916f615207eeb079235c5db16a513996d5
Hp <sarvina107@gmail.com>
2026-05-19 16:20:00 +0500

    Fix error

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import './i18n';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
