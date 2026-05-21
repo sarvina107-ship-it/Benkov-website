c783d95503fe3b311493b2c575e715e0df11dfd1
Hp <sarvina107@gmail.com>
2026-05-21 14:36:17 +0500

    feat: add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg}'],
+      },
+      manifest: {
+        name: 'Республиканская специализированная художественная школа имени П. Бенькова',
+        short_name: 'Школа Бенькова',
+        description: 'Официальный сайт Художественной Школы имени П. Бенькова в Ташкенте',
+        theme_color: '#0E1A2B',
+        background_color: '#ffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
